2f62b2ced394f1d53577f1328da6a6834ab59da3
Mohit Bhavsar <64053968+Mohitbhavsar707@users.noreply.github.com>
2023-01-19 15:25:53 -0500

    Items can be cleared now

--- main.js
+++ main.js
@@ -81,7 +81,10 @@ const mainMenuTemplate = [
                 }
             },
             {
-                label: 'Clear Items'
+                label: 'Clear Items',
+                click(){
+                    mainWindow.webContents.send('item:clear');
+                }
             },
             {
                 label: 'Quit', 
